4c72fc264b3bf3af8aff1ac2b37fac7c06c5754e
Harsha <kharsha9990@gmail.com>
2026-06-04 02:47:56 -0400

    Add SQL query to find consecutive numbers

---
+++ 180. Consecutive_numbers.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num,
+           id - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS grp
+    FROM Logs
+) t
+GROUP BY num, grp
+HAVING COUNT(*) >= 3;
